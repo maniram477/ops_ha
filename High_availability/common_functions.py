cfa967b3618556dc06bc7b6f86b6d37d1e49a219
maniram477 <maniram477@gmail.com>
2016-08-30 20:46:11 +0530

    instance migration bug fix122

--- High_availability/common_functions.py
+++ High_availability/common_functions.py
@@ -455,7 +455,8 @@ def message_queue(dhost=None,task=None):
                 try:
                     zk.create("/openstack_ha/instances/pending/" + dhost+"/"+instance_list[i])
                     zk.delete("/openstack_ha/instances/down_instances/" + dhost + "/" + instance_list[i],recursive=True)
-                    task.apply_async((instances_list[i],), queue='mars', countdown=5)
+                    instance_string = str(instances_list[i])
+                    task.apply_async((instance_string,), queue='mars', countdown=5)
                 except Exception as e:
                     print(e)
 
