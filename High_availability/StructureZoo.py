62c67ebc34390bed526a4c2831a6f0f6e411e12f
maniram477 <maniram477@gmail.com>
2016-08-30 14:08:10 +0530

    Removed Code duplication

--- High_availability/StructureZoo.py
+++ High_availability/StructureZoo.py
@@ -64,34 +64,3 @@ def leaderCheck(zk=zk):
             leader=i
     leader=leader[0:-10]
     return(leader)
-
-
-def instance_migration(dhosts):
-    for dhost in dhosts:
-            if(zk.exists("/openstack_ha/instances/down_instances" + dhost)==False):
-                zk.create("/openstack_ha/instances/down_instances" + dhost, b"a value", None, True)
-                for instance_obj in list_instances(dhost):
-                    # Addon-Feature
-                    # Can Add another check to only select instances which have HA option enabled
-                    # print(instance_obj.id)
-                    zk.create("/openstack_ha/instances/down_instances/" + dhost+"/"+instance_obj.id, b"a value", None, True)
-                    #create instance detatils under the down_instances in zookeepr
-        message_queue(dhost)
-
-def message_queue(dhost=dhost):
-    instance_list=zk.get_children("/openstack_ha/instances/down_instances/" + dhost)
-    if(len(instance_list)!=0):
-        pending_instances_list=zk.get_children("/openstack_ha/instances/pending/"+dhost)
-        instance_list = zk.get_children("/openstack_ha/instances/down_instances/" + dhost)
-        if(pending_instances_list<10):
-            add_pending_instance_list=10-len(pending_instances_list)
-            for i in range(add_pending_instance_list):
-                try:
-                    zk.create("/openstack_ha/instances/pending/" + dhost+"/"+instance_list[i])
-                    zk.delete("/openstack_ha/instances/down_instances/" + dhost + "/" + instance_list[i],recursive=True)
-                    migrate.apply_async((instance_list[i],), queue='mars', countdown=wait_time)
-                except Exception as e:
-                    print(e)
-    else:
-        if (zk.exists("/openstack_ha/hosts/down/" + dhost) == False):
-            zk.create("/openstack_ha/hosts/down/" + dhost, b"a value", None, True)
